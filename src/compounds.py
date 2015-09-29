0f315bee7052a61a8f11cce4ce1aba7b38016681
julieweeds <julie.weeds@gmail.com>
2015-09-29 14:23:26 +0100

    Lots of low counts due not many not being nn

--- src/compounds.py
+++ src/compounds.py
@@ -18,17 +18,36 @@ class Compound:
     def setJudgements(self,NCs):
         self.judgements=[int(item) for item in NCs]
 
-    def setRel(self,rels):
+    def setRel(self,rel):
         self.relmap={}
-        for rel in rels:
-            dep=rel[1]
-            self.relmap[dep]=rel
+        dep=rel[1]
+        self.relmap[dep]=rel
+
+        self.deps=[]
+        self.heads=[]
+        for rel in self.relmap.values():
+            self.deps.append(rel[1])
+            if rel[2] not in self.deps:self.heads.append(rel[2])
+
 
     def setPos(self,poses):
-        self.posmap={}
-        for pos in poses:
-            self.posmap[pos[1]]=pos
+        self.posmap=poses
 
+
+    def getHead(self):
+
+        if len(self.heads)==1:
+            return self.name.split(" ")[self.heads[0]-1]
+        else:
+            print "Error: multiple heads"
+            return self.name.split(" ")[self.heads[0]-1]
+
+    def getDeps(self):
+        return [self.name.split(" ")[dep-1] for dep in self.deps]
+
+    def match(self,head,dep):
+
+        return head == self.getHead() and dep in self.getDeps()
 
     def getScore(self):
         return sum(self.judgements)
@@ -58,35 +77,44 @@ class Compounder:
         with open(self.configfile) as fp:
             self.configured=yaml.safe_load(fp)
         print self.configured
-        self.compounds=[]
+        self.compounds={} #key is head, value is list of compounds
 
         try:
-            self.rel=self.configured.get("rels",[("nn",1,2)])
-            self.pos=self.configured.get("pos",[])
+            self.rel=self.configured.get("rels",["nn",1,2])
+            self.posmap=self.configured.get("pos",[])
             self.compound_file=self.configured["compound_file"]
+
+            print self.posmap
+
         except:
             print "Error: problem with configuration"
 
     def readcompounds(self):
 
-        try:
-            with open(self.compound_file) as fp:
-                for line in fp:
-                    line=line.rstrip()
-                    fields=line.split(",")
-                    aCompound=Compound(fields[0])
-                    aCompound.setRel(self.rel)
-                    aCompound.setPos(self.pos)
-                    aCompound.setJudgements(fields[1:])
-                    self.compounds.append(aCompound)
+        with open(self.compound_file) as fp:
+            for line in fp:
+                line=line.rstrip()
+                fields=line.split(",")
+                aCompound=Compound(fields[0])
+                aCompound.setRel(self.rel)
+                aCompound.setPos(self.posmap)
+                aCompound.setJudgements(fields[1:])
+                head=aCompound.getHead()
+                if self.compounds.get(head,None) != None:
+                    sofar=self.compounds[head]
+                else:
+                    sofar=[]
 
-        except:
-            print "Error: problem reading compound file"
+                sofar.append(aCompound)
+                self.compounds[head]=sofar
+
+
 
     def run(self):
         self.readcompounds()
-        for compound in self.compounds:
-            compound.display()
+        for head in self.compounds.keys():
+            for compound in self.compounds[head]:
+                compound.display()
         return
 
 if __name__=="__main__":
--- src/observations.py
+++ src/observations.py
@@ -3,25 +3,96 @@ __author__ = 'juliewe'
 import sys
 from compounds import Compounder, Compound
 
+
+def getPos(word):
+    return word.split('/')[1]
+
+def getLex(word):
+    return word.split('/')[0].lower()
+
 class CompoundFinder(Compounder):
 
     def __init__(self,configfile):
         Compounder.__init__(self,configfile)
-        self.corpusfile=self.configured.get("corpus_file")
+        self.corpusfile=self.configured.get("corpus")
+        self.readcompounds()
+        self.counts={}
+        for head in self.compounds.keys():
+            for comp in self.compounds[head]:
+                self.counts[comp]=0
 
-        frequencies={}
-        for comp in self.compounds:
-            frequencies[comp]=0
 
     def process_corpus(self):
         with open(self.corpusfile) as fp:
+            sentencebuffer={}
+            lines=0
             for line in fp:
                 #need to load in sentence at a time and process each sentence
                 #need to be careful in case there are any dependencies on dependent
                 line=line.rstrip()
-        return
+                fields=line.split('\t')
+                if len(fields)==4:
+                    sentencebuffer[fields[0]]=fields[1:]
+                else:
+                    self.process_sentence(sentencebuffer)
+                    sentencebuffer={}
+                lines+=1
+                if lines%1000000==0: print "Processed "+str(lines)+" lines"
+        #analyse counts
+
+        mini = min(self.counts.values())
+        maxi = max(self.counts.values())
+        print "Min count is "+str(mini)
+        print "Max count is "+str(maxi)
+        print self.counts.keys()
+
+        minicounts=0
+        for comp in self.counts.keys():
+            if self.counts[comp]<mini+5:
+                minicounts+=1
+
+                for line in comp.getConll():
+                    print line
+                print self.counts[comp]
+            elif self.counts[comp]>maxi-5:
+
+                for line in comp.getConll():
+                    print line
+                print self.counts[comp]
+
+        print "Low counts: "+str(minicounts)
+        #for key in self.counts.keys():
+         #   for line in key.getConll():
+          #      print line
+           # print self.counts[key]
+
+    def process_sentence(self,sentence):
+
+        rel_to_match=self.rel[0]  #assuming there is only one rel in list at the moment
+        dp=str(self.rel[1])
+        hp=str(self.rel[2])
+
+        for arc in sentence.values():
+            if arc[2]==rel_to_match:
+
+                dep = arc[0]
+                head=sentence[arc[1]][0]
+                if getPos(dep).startswith(self.posmap[dp]) and getPos(head).startswith(self.posmap[hp]):
+
+                    dep=getLex(dep)
+                    head=getLex(head)
+
+                    for comp in self.compounds.get(head,[]):
+
+                        if comp.match(head,dep):
+                            sofar=self.counts[comp]
+
+                            self.counts[comp]=sofar+1
+        #print self.compounds.keys()
+
 
     def run(self):
+
         self.process_corpus()
 
 
